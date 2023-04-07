b632e831aaa600b0847e3f580bb41aabead5a48d
mruros <uros23.m@gmail.com>
2023-04-07 06:04:10 +0200

    test

--- app/src/rgb_underglow.c
+++ app/src/rgb_underglow.c
@@ -200,12 +200,12 @@ void simulate_rgb_keypress(uint32_t keycode) {
 }
 
 static int zmk_rgb_underglow_layer_state_change_listener(const zmk_event_t *eh) {
-    if(eh->state == true) {
-        for(int i = 0; i < eh->layer; i++) {
+    if(as_zmk_layer_state_changed(eh)->state == true) {
+        for(int i = 0; i < as_zmk_layer_state_changed(eh)->layer; i++) {
             simulate_rgb_keypress(RGB_HUI);
         }
     } else {
-        for(int i = 0; i < eh->layer; i++) {
+        for(int i = 0; i < as_zmk_layer_state_changed(eh)->layer; i++) {
             simulate_rgb_keypress(RGB_HUD);
         }
     }
